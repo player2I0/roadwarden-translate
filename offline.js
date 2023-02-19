c9ca91a6db5bf408cb4965db71942f3764c12033
player2I0 <59975613+player2I0@users.noreply.github.com>
2023-02-19 19:28:58 +0100

    init

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1676824056,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/block-sheet0.png",
+		"images/block-sheet1.png",
+		"images/notif-sheet0.png",
+		"images/notif-sheet1.png",
+		"images/dictblock-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
