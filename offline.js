e7ceb3e009b1896b34c5f0713f8d74b51356469f
player2I0 <59975613+player2I0@users.noreply.github.com>
2023-06-04 11:56:24 +0200

    1.4

--- offline.js
+++ offline.js
@@ -1,20 +1,26 @@
 ﻿{
-	"version": 1677968534,
+	"version": 1685872503,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
-		"images/block-sheet0.png",
-		"images/block-sheet1.png",
-		"images/notif-sheet0.png",
-		"images/notif-sheet1.png",
-		"images/dictblock-sheet0.png",
+		"images/gl-sheet0.png",
+		"images/pl-sheet0.png",
+		"images/p-sheet0.png",
+		"images/sel-sheet0.png",
+		"images/dictbg-sheet0.png",
+		"images/slider-sheet0.png",
+		"images/slidertrail-sheet0.png",
+		"images/slidertrail-sheet1.png",
+		"images/blur-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"hh.txt",
+		"style.css"
 	]
 }
